e020399dad5d63435fdb50cedf9b494cadcf35d2
Elviani21 <elpianielpiani23@gmail.com>
2024-10-28 14:49:26 +0700

    Elviani_F1G123044_Ujianrpl

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="UTF_8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <script src="js/bootstrap.bundle.min.js"></script>
